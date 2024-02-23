3744376aef771d450a8e474e0a22abe42bdb0ccc
Fenriz1349 <129265577+Fenriz1349@users.noreply.github.com>
2024-02-23 09:02:42 +0100

    Initial Commit

---
+++ CalculaSwift/CalculaSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculaSwiftApp.swift
+//  CalculaSwift
+//
+//  Created by apprenant68 on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculaSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
